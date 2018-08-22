34ae3a2a2fb9ab20df33c73ae9b372b086572fdd
daisukenagata <dbank0208@gmail.com>
2018-08-23 04:16:09 +0900

    add

---
+++ AllLabel.swift
@@ -0,0 +1,94 @@
+import UIKit
+
+
+public let formatter: NumberFormatter = NumberFormatter()
+
+@IBDesignable
+final class AllLabel: UILabel {
+
+    var textInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat = 0.0
+    @IBInspectable var borderColor: UIColor = UIColor.clear
+    @IBInspectable var borderWidth: CGFloat = 0.0
+    @IBInspectable var priceCount: Int = 0
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        guard priceCount == 0 else {
+            self.text = priceCount.JPYString
+            return
+        }
+    }
+
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = UIEdgeInsetsInsetRect(bounds, textInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textInsets.top,
+                                          left: -textInsets.left,
+                                          bottom: -textInsets.bottom,
+                                          right: -textInsets.right)
+        
+        return UIEdgeInsetsInsetRect(textRect, invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = (cornerRadius > 0)
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderWidth = borderWidth
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, textInsets))
+    }
+
+}
+
+extension EdgeInsetLabel {
+
+    @IBInspectable
+    var topTextInset: CGFloat {
+        set { textInsets.top = newValue }
+        get { return textInsets.top }
+    }
+    
+    @IBInspectable
+    var leftTextInset: CGFloat {
+        set { textInsets.left = newValue }
+        get { return textInsets.left }
+    }
+    
+    @IBInspectable
+    var bottomTextInset: CGFloat {
+        set { textInsets.bottom = newValue }
+        get { return textInsets.bottom }
+    }
+    
+    @IBInspectable
+    var rightTextInset: CGFloat {
+        set { textInsets.right = newValue }
+        get { return textInsets.right }
+    }
+}
+
+public extension Int {
+    private func formattedString(style: NumberFormatter.Style, localeIdentifier: String) -> String {
+        formatter.numberStyle = style
+        formatter.locale = Locale(identifier: localeIdentifier)
+        return formatter.string(from: self as NSNumber) ?? ""
+    }
+
+    // カンマ区切りString
+    var formattedJPString: String {
+        return formattedString(style: .decimal, localeIdentifier: "ja_JP")
+    }
+
+    // 日本円表記のString
+    var JPYString: String {
+        return formattedString(style: .currency, localeIdentifier: "ja_JP")
+    }
+
+    // USドル表記のString
+    var USDString: String {
+        return formattedString(style: .currency, localeIdentifier: "en_US")
+    }
+}
